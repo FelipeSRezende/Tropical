fadf18e91bb0580f025312865ddbdf1ffe66f6c4
Maique Rosa <maique.rosa@gmail.com>
2017-04-20 18:29:05 -0300

    Commit Master: Alterando o settings.py para o pythonanywhere

--- tropical/settings.py
+++ tropical/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'jkiv=bez1@wb&i*xh^1%+-5y!55+8^80q3xuq8*bszv$ag#oce'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1","localhost"]
 
@@ -123,5 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 LOGIN_REDIRECT_URL = '/home/'
 LOGIN_URL = '/login/'
